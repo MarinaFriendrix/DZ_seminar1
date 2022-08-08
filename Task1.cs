b33203cc5eb9275dd0208ff58d359491df702d9d
MarinaFriendrix <109303245+MarinaFriendrix@users.noreply.github.com>
2022-08-08 15:39:51 +0300

    Задача 2

--- Task1.cs
+++ Task1.cs
@@ -1,21 +1,48 @@
 ﻿// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+// Console.Write("Введите число 1: ");
+// double num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// double num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1==num2)
+// {
+//     Console.WriteLine("числа равны");
+
+// }
+// else
+// {
+//     if(num1>num2)
+//     Console.WriteLine("Число 1 больше Числа 2");
+//     else
+//     Console.WriteLine("Число 2 больше Числа 1");
+
+// }
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
 Console.Write("Введите число 1: ");
-double num1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2: ");
-double num2 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1==num2)
+
+if(num1>num2 && num2>num3)
 {
-    Console.WriteLine("числа равны");
-
+    Console.WriteLine(num1);
 }
 else
 {
-    if(num1>num2)
-    Console.WriteLine("Число 1 больше Числа 2");
+    if(num1<num2 && num2>num3)
+    {
+       Console.WriteLine(num2); 
+    }
     else
-    Console.WriteLine("Число 2 больше Числа 1");
-    
+    {
+      Console.WriteLine(num3);  
+    }
 }
-
